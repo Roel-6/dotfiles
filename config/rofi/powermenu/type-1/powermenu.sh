461bb8b7c992ab7142fe651312c3712a92b3496b
Roel_6 <zatokamishiro@gmail.com>
2026-02-02 21:01:51 +0700

    reworked picom configs to make it my own

--- config/rofi/powermenu/type-1/powermenu.sh
+++ config/rofi/powermenu/type-1/powermenu.sh
@@ -88,7 +88,7 @@ case ${chosen} in
         ;;
     $lock)
 		if [[ -x '/usr/local/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
+			betterlockscreen -l 
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
 		fi
